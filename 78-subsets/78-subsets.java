0ff9c30a0423328328b2ec53213f484747dd691f
xYesh <s.yesh96@gmail.com>
2022-07-02 18:16:32 +0530

    Time: 1 ms (88.25%), Space: 43.4 MB (51.58%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,35 +1,52 @@
 class Solution {
     
-    List<List<Integer>> sol;
+//     List<List<Integer>> sol;
+//     public List<List<Integer>> subsets(int[] nums) {
+//         sol = new ArrayList<>();
+//         for (int i=0; i<nums.length+1; i++) {
+//             helper(0,nums,new ArrayList(),i);
+//         }
+//         return sol;
+//     }
+    
+//     private void helper(int start, int[] nums, List<Integer> curr, int size) {
+//         if (curr.size() == size) {
+//             sol.add(new ArrayList(curr));
+//             return;
+//         }
+        
+//         for (int i = start; i<nums.length; i++) {
+//             curr.add(nums[i]);
+//             helper(i+1,nums,curr,size);
+//             curr.remove(curr.size()-1);
+//         }
+//         return;
+//     }
+    
     
     public List<List<Integer>> subsets(int[] nums) {
-        sol = new ArrayList<>();
         
-        for (int i=0; i<nums.length+1; i++) {
-            helper(0,nums,new ArrayList(),i);
+        List<List<Integer>> sol = new ArrayList<>();
+        sol.add(new ArrayList());
+        
+        for (int num : nums) {
+            List<List<Integer>> curr = new ArrayList<>();
+            
+            for (List<Integer> temp: sol) {
+                List<Integer> ntemp = new ArrayList<>(temp);
+                ntemp.add(num);
+                curr.add(ntemp);
+            }
+            
+            for (List<Integer> temp: curr) {
+                sol.add(temp);
+            }            
         }
         
         return sol;
+        
     }
     
-    private void helper(int start, int[] nums, List<Integer> curr, int size) {
-        
-        if (curr.size() == size) {
-            sol.add(new ArrayList(curr));
-            return;
-        }
-        
-        for (int i = start; i<nums.length; i++) {
-            
-            curr.add(nums[i]);
-            
-            helper(i+1,nums,curr,size);
-            
-            curr.remove(curr.size()-1);
-                        
-        }
-        
-        return;
-         
-    }
+    
+    
 }
