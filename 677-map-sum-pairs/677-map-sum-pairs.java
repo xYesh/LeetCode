1abb9309d6dbcd339619e4add78b160a4e2f9c9b
xYesh <s.yesh96@gmail.com>
2022-07-13 22:04:31 +0530

    Time: 27 ms (15.24%), Space: 43.5 MB (23.11%) - LeetHub

--- 677-map-sum-pairs/677-map-sum-pairs.java
+++ 677-map-sum-pairs/677-map-sum-pairs.java
@@ -1,31 +1,51 @@
 class MapSum {
-    HashMap<String, Integer> map;
-    TrieNode root;
+
+    HashMap<String,Integer> map;
+    TrieNode head;
     public MapSum() {
         map = new HashMap();
-        root = new TrieNode();
+        head = new TrieNode();
     }
+    
     public void insert(String key, int val) {
-        int delta = val - map.getOrDefault(key, 0);
-        map.put(key, val);
-        TrieNode cur = root;
-        cur.score += delta;
-        for (char c: key.toCharArray()) {
-            cur.children.putIfAbsent(c, new TrieNode());
-            cur = cur.children.get(c);
-            cur.score += delta;
+        int delta = val - map.getOrDefault(key,0);
+        map.put(key,val);
+        head.score = head.score + delta;
+        
+        TrieNode curr = head;
+        for (char ch : key.toCharArray()) {
+            curr.children.putIfAbsent(ch, new TrieNode());
+            curr.children.get(ch).score = delta + curr.children.get(ch).score;
+            curr = curr.children.get(ch);           
         }
     }
+    
     public int sum(String prefix) {
-        TrieNode cur = root;
-        for (char c: prefix.toCharArray()) {
-            cur = cur.children.get(c);
-            if (cur == null) return 0;
+        
+        TrieNode curr = head;
+        int score = head.score;
+        for (char ch : prefix.toCharArray()) {
+            if (!curr.children.containsKey(ch)) {
+                return 0;
+            }
+            score = curr.children.get(ch).score;
+            curr = curr.children.get(ch);
         }
-        return cur.score;
+        return score;
+         
+    }
+    
+    private class TrieNode {
+        
+        int score;
+        HashMap<Character,TrieNode> children = new HashMap();
+        
     }
 }
-class TrieNode {
-    Map<Character, TrieNode> children = new HashMap();
-    int score;
-}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
