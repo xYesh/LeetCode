a57e375bcdcb60f592e54df1e0972f0875d722ba
xYesh <s.yesh96@gmail.com>
2022-07-14 18:05:49 +0530

    Time: 445 ms (5.06%), Space: 54.6 MB (5.06%) - LeetHub

---
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int shortestWay(String source, String target) {
+        
+        int count = 0;
+        
+        for (char ch : target.toCharArray()) {
+            if (!source.contains(ch+"")) {
+                return -1;
+            }            
+        }
+        
+        for (int i=0; i<target.length();) {
+                        
+            for (int j = 0 ; j < source.length() && i <target.length(); j++) {
+                char tch = target.charAt(i);
+                char sch = source.charAt(j);
+                System.out.println(tch + " " + sch);
+                if (tch == sch) {
+                    i++;
+                }                
+            }
+            
+            count ++;           
+        }
+        
+        return count;
+    }
+}
