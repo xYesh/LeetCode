0d456ec49dcc6b2afd2df3fff4ae139b1ee7ce78
xYesh <s.yesh96@gmail.com>
2022-07-16 09:27:59 +0530

    Time: 17 ms (63.54%), Space: 50.3 MB (60.54%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,26 +1,45 @@
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
+
         HashMap<Integer,Integer> map = new HashMap();
-        
+
         for (int num: nums) {
             map.put(num,map.getOrDefault(num,0) + 1);
         }
-        
-        // System.out.println(map);
-        
-        PriorityQueue<Integer> q = new PriorityQueue((n1,n2) -> map.get(n2) - map.get(n1));
-        
-        for (Map.Entry<Integer,Integer> entry: map.entrySet()) {
-            q.add(entry.getKey());
+
+        ArrayList<Integer>[] bucket = new ArrayList[nums.length+1];
+
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if (bucket[entry.getValue()] == null) {
+                bucket[entry.getValue()] = new ArrayList<>();
+            }
+
+            bucket[entry.getValue()].add(entry.getKey());
         }
-        
+
+        int tempk = k;
         int[] res = new int[k];
-        for (int i=0;i<k;i++) {
-            res[i] = q.poll();
+        int counter = 0;
+        for (int i=nums.length; i>=0; i--) {
+
+            if (bucket[i] != null) {
+                ArrayList<Integer> temp = bucket[i];
+                
+                for (int t:temp) {
+                    if (counter == k) {
+                        break;
+                    }
+                    res[counter] = t;
+                    counter++;
+
+                }
+            }
+
         }
-        
+
         return res;
-        
+
     }
 }
