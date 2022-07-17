0501ec16a9c4d3d6dcd8f4a673d8517990cf9c91
xYesh <s.yesh96@gmail.com>
2022-07-17 12:27:53 +0530

    Time: 1 ms (62.76%), Space: 42 MB (55.92%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public int maxDistance(int[] colors) {
+//         int sol = 0;
+//         int currColor = -1;
+//         for (int i=0; i<colors.length; i++) {
+//             currColor = colors[i];
+//             for (int j=i+1;j<colors.length;j++) {
+//                 if (colors[j] != currColor) {
+//                     sol = Math.max(sol, j-i);
+//                 }
+//             }
+//         }
+//         return sol;
+//     }
+// }
+
 class Solution {
-    public int maxDistance(int[] colors) {
-        int sol = 0;
-        int currColor = -1;
-        for (int i=0; i<colors.length; i++) {
-            currColor = colors[i];
-            for (int j=i+1;j<colors.length;j++) {
-                if (colors[j] != currColor) {
-                    sol = Math.max(sol, j-i);
-                }
-            }
-        }
-        return sol;
+    public int maxDistance(int[] cs) {
+        int n = cs.length, i = 0, j = n - 1;
+        while (cs[0] == cs[j])
+            --j;
+        while (cs[n - 1] == cs[i])
+            ++i;
+        return Math.max(j, n - i - 1);     
     }
 }
