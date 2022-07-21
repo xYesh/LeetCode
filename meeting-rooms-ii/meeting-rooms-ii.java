6954e06088be5130d9a35c6103c480b20b020ede
xYesh <s.yesh96@gmail.com>
2022-07-21 08:24:54 +0530

    Time: 10 ms, Memory: 46 MB - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+        q.add(intervals[0][1]);
+        
+        for (int i=1; i<intervals.length; i++) {
+            
+            int[] curr = intervals[i];
+            
+            if (curr[0] >= q.peek()) {
+                q.poll();
+            }
+            q.add(curr[1]);
+        }
+        
+        return q.size();
+    }
+}
