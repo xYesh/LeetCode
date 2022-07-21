d07f743397a2679d2526da9430e499b96cb641be
xYesh <s.yesh96@gmail.com>
2022-07-21 21:20:16 +0530

    Time: 244 ms (5.07%), Space: 100.9 MB (17.64%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,29 +1,26 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0) return 0;
+        
+        Arrays.sort(points, (a,b) -> a[0] - b[0]);
 
-        // sort by x_end
         Arrays.sort(points, (o1, o2) -> {
-            // We can't simply use the o1[1] - o2[1] trick, as this will cause an 
-            // integer overflow for very large or small values.
             if (o1[1] == o2[1]) return 0;
             if (o1[1] < o2[1]) return -1;
             return 1;
         });
-
-        int arrows = 1;
-        int xStart, xEnd, firstEnd = points[0][1];
-        for (int[] p: points) {
-            xStart = p[0];
-            xEnd = p[1];
-            // if the current balloon starts after the end of another one,
-            // one needs one more arrow
-            if (firstEnd < xStart) {
-                arrows++;
-                firstEnd = xEnd;
+        
+        int currMax = points[0][1];
+        int noOfArrows = 1;
+        
+        for (int i=0; i<points.length; i++) {
+            // System.out.println(points[i][0] + " > " + currMax);
+            if (points[i][0] > currMax) {
+                noOfArrows ++;
+                currMax = points[i][1];
             }
         }
-
-        return arrows;
+        
+        return noOfArrows;
+        
     }
 }
