cea330a185a9958ada611f82aac23206bcca3b0f
xYesh <s.yesh96@gmail.com>
2022-07-26 21:24:06 +0530

    Time: 7 ms (33.71%), Space: 50 MB (98.14%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,43 +1,45 @@
 class Solution {
-
-    private List<int[]> directions = Arrays.asList(
-        new int[] {1,0},
-        new int[] {-1,0},
-        new int[] {0,1},
-        new int[] {0,-1}
-    );
-    
-    public int numIslands(char[][] grid) {
-        
-        LinkedList<int[]> q = new LinkedList<>();
-        
-        int sol = 0;
-        
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    sol++;
-                    q.add(new int[]{i,j});
-                    
-                    while (!q.isEmpty()) {
-                        int[] curr = q.poll();
-                        int row = curr[0];
-                        int col = curr[1];
-                        
-                        if (row < 0 || row >= grid.length || col < 0 || col >=grid[0].length || grid[row][col] != '1') {
-                            continue;
-                        }
-                        
-                        grid[row][col] = '0';
-                        for (int[] direction : directions) {
-                            int r = row + direction[0];
-                            int c = col + direction[1];
-                            q.add(new int[] {r,c});
-                        }
-                    }
-                }
-            }
-        }
-        return sol;
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
     }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          grid[r][c] = '0'; // mark as visited
+          Queue<Integer> neighbors = new LinkedList<>();
+          neighbors.add(r * nc + c);
+          while (!neighbors.isEmpty()) {
+            int id = neighbors.remove();
+            int row = id / nc;
+            int col = id % nc;
+            if (row - 1 >= 0 && grid[row-1][col] == '1') {
+              neighbors.add((row-1) * nc + col);
+              grid[row-1][col] = '0';
+            }
+            if (row + 1 < nr && grid[row+1][col] == '1') {
+              neighbors.add((row+1) * nc + col);
+              grid[row+1][col] = '0';
+            }
+            if (col - 1 >= 0 && grid[row][col-1] == '1') {
+              neighbors.add(row * nc + col-1);
+              grid[row][col-1] = '0';
+            }
+            if (col + 1 < nc && grid[row][col+1] == '1') {
+              neighbors.add(row * nc + col+1);
+              grid[row][col+1] = '0';
+            }
+          }
+        }
+      }
+    }
+
+    return num_islands;
+  }
 }
