8c8732bd2545025c168658bd881508311874e40a
xYesh <s.yesh96@gmail.com>
2022-07-30 10:45:17 +0530

    Time: 1 ms (88.57%), Space: 41.4 MB (82.54%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,40 +1,32 @@
 class Solution {
-    public String decodeString(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == ']') {
-                
-                List<Character> str = new ArrayList();
-                
-                while (stack.peek() != '[') {
-                    str.add(stack.pop());
+    String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                // push the number k to countStack
+                countStack.push(k);
+                // push the currentString to stringStack
+                stringStack.push(currentString);
+                // reset currentString and k
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                // decode currentK[currentString] by appending currentString k times
+                for (int currentK = countStack.pop(); currentK > 0; currentK--) {
+                    decodedString.append(currentString);
                 }
-                
-                stack.pop();
-                
-                int count = 0;
-                int base = 1;
-                while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
-                    count = count + (stack.pop() - '0') * base; base = base * 10;
-                }
-                
-                for (int k=0; k<count; k++) {
-                    for (int j=str.size()-1; j>=0; j--) {
-                        stack.add(str.get(j)); 
-                    }
-                }
+                currentString = decodedString;
             } else {
-                stack.add(ch);           
+                currentString.append(ch);
             }
         }
-        
-        // get the result from stack
-        char[] result = new char[stack.size()];
-        for (int i = result.length - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
-        return new String(result);
-        
+        return currentString.toString();
     }
 }
+
