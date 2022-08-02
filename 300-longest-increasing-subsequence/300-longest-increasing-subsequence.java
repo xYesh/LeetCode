0a98c84d013eb024f9d517583d993c2fc2a25657
xYesh <s.yesh96@gmail.com>
2022-08-02 21:24:46 +0530

    Time: 98 ms (29.48%), Space: 44.8 MB (32.42%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+        int[] dp = new int[nums.length + 1];
+        
+        Arrays.fill(dp,1);
+        
+        for (int i=1; i<nums.length; i++) {
+            for (int j=0; j<i; j++) {
+                // System.out.println("checking for " + nums[i]);
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        int max = -1;
+        for (int i : dp) {
+            max = Math.max(max,i);
+        }
+        
+        // System.out.println(Arrays.toString(dp));
+        
+        return max;
+        
+    }
+}
